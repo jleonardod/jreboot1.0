7074eacdda0c635d42af20fdf14c0fe621e00397
Leonardo Diaz <jldiaz9623@gmail.com>
2023-02-08 13:47:51 -0500

    Seccion Opinión

--- src/components/sub-components/bloqueSecundario.jsx
+++ src/components/sub-components/bloqueSecundario.jsx
@@ -25,7 +25,7 @@ const BloqueSecundario = () => {
     setShowAudios(true)
     console.log("Audios Habilitada")
   }
-  
+
   return(
     <div className="grid grid-cols-1 relative md:col-span-3 md:grid-cols-1 gap-0">
       <nav
@@ -34,19 +34,75 @@ const BloqueSecundario = () => {
         <a 
           href="#" 
           onClick={habilitarOpinion}
-          className={`hover:bg-primary transition-colors duration-300 py-3 px-5 ${ showOpinion ? "border-b-4 border-fourth" : "border-none"} h-full`}
+          className={`hover:bg-primary transition-colors duration-300 py-3 px-5 
+          ${ showOpinion ? "border-b-4 border-fourth" : "border-none"} h-full`}
         >
           Opinión
         </a>
-        <a href="#" onClick={habilitarSocial} className={`hover:bg-primary transition-colors duration-300 py-3 px-5 ${ showSocial ? "border-b-4 border-fourth" : "border-none"} h-full`}>
+        <a 
+          href="#" 
+          onClick={habilitarSocial} 
+          className={`hover:bg-primary transition-colors duration-300 py-3 px-5 
+          ${ showSocial ? "border-b-4 border-fourth" : "border-none"} h-full`}
+        >
           Social
         </a>
-        <a href="#" onClick={habilitarAudios} className={`hover:bg-primary transition-colors duration-300 py-3 px-5 ${ showAudios ? "border-b-4 border-fourth" : "border-none"} h-full`}>
+        <a 
+          href="#" 
+          onClick={habilitarAudios} 
+          className={`hover:bg-primary transition-colors duration-300 py-3 px-5 
+          ${ showAudios ? "border-b-4 border-fourth" : "border-none"} h-full`}
+        >
           Audios
         </a>
       </nav>
-      <div className={`mt-[3rem] p-3 h-full ${ showOpinion && !showSocial && !showAudios ? "block" : "hidden" }`}>
-        Opinion
+      <div className={`mt-[3rem] h-full ${ showOpinion && !showSocial && !showAudios ? "block" : "hidden" }`}>
+        <nav className="w-full gap-4">
+          <div className="w-full p-4 pl-4 pb-2 hover:bg-gray-100 transition-color duration-300 bg-blue-200 flex flex-row gap-3 bg-white border-b-2 border-gray-300">
+            <img 
+              src="https://www.elespectador.com/resizer/Z-Z_SkaZNyKzi8GrtbuhUTk68xg=/120x120/filters:quality(80):format(jpeg)/s3.amazonaws.com/arc-authors/elespectador/18f1e866-7cd2-4ffd-9939-03e2c5bd68fb.png"
+              className="w-20 h-20 object-cover rounded-full ring-2 ring-gray-300 cursor-pointer"/>
+            <div>
+              <a href="#" className="text-[1rem] hover:underline transition-all duration-200 font-black">¿Qué es la mutilación genital femenina y cómo acabar con esta práctica?</a>
+              <br />
+              <a href="#" className="font-semibold text-[0.8rem]">Las Igualadas</a>
+              <p className="text-gray-500 text-[0.8rem]">6 feb. 2023 - 12:00 p. m</p>
+            </div>
+          </div>
+          <div className="w-full p-4 pl-4 pb-2 hover:bg-gray-100 transition-color duration-300 bg-blue-200 flex flex-row gap-3 bg-white border-b-2 border-gray-300">
+            <img 
+              src="https://www.elespectador.com/resizer/zwsP1b0EnEy49x-TXvQ9vBDQnDw=/120x120/filters:quality(80):format(jpeg)/s3.amazonaws.com/arc-authors/elespectador/39e31163-ba9b-4c30-a9a6-13e6cff17d12.png"
+              className="w-20 h-20 object-cover rounded-full ring-2 ring-gray-300 cursor-pointer"/>
+            <div>
+              <a href="#" className="text-[1rem] hover:underline transition-all duration-200 font-black">Columna de Antonio Casale: La sede de la selección</a>
+              <br />
+              <a href="#" className="font-semibold text-[0.8rem]">Antonio Casale</a>
+              <p className="text-gray-500 text-[0.8rem]">6 feb. 2023 - 12:00 p. m</p>
+            </div>
+          </div>
+          <div className="w-full p-4 pl-4 pb-2 hover:bg-gray-100 transition-color duration-300 bg-blue-200 flex flex-row gap-3 bg-white border-b-2 border-gray-300">
+            <img 
+              src="https://www.elespectador.com/resizer/K0ZGoBDeKgEpLc2pOvo7SK7z8gg=/120x120/filters:quality(80):format(jpeg)/s3.amazonaws.com/arc-authors/elespectador/e28afff8-99e3-4313-af10-eda5917910ff.png"
+              className="w-20 h-20 object-cover rounded-full ring-2 ring-gray-300 cursor-pointer"/>
+            <div>
+              <a href="#" className="text-[1rem] hover:underline transition-all duration-200 font-black">¿Libertad de expresión o irresponsabilidad?</a>
+              <br />
+              <a href="#" className="font-semibold text-[0.8rem]">Cartas de los lectores</a>
+              <p className="text-gray-500 text-[0.8rem]">5 feb. 2023 - 9:05 p. m.</p>
+            </div>
+          </div>
+          <div className="w-full p-4 pl-4 pb-2 hover:bg-gray-100 transition-color duration-300 bg-blue-200 flex flex-row gap-3 bg-white border-b-2 border-gray-300">
+            <img 
+              src="https://www.elespectador.com/resizer/MuXRIhopVblpEZ_ZGUj8xuLDBR4=/120x120/filters:quality(80):format(jpeg)/s3.amazonaws.com/arc-authors/elespectador/0269a791-dccf-45da-a1db-6cfc45e8c27f.png"
+              className="w-20 h-20 object-cover rounded-full ring-2 ring-gray-300 cursor-pointer"/>
+            <div>
+              <a href="#" className="text-[1rem] hover:underline transition-all duration-200 font-black">Ponernos al día o cambiar</a>
+              <br />
+              <a href="#" className="font-semibold text-[0.8rem]">Fernando Barbosa</a>
+              <p className="text-gray-500 text-[0.8rem]">5 feb. 2023 - 9:05 p. m</p>
+            </div>
+          </div>
+        </nav>
       </div>
       <div className={`mt-[3rem] p-3 h-full ${ showSocial && !showOpinion && !showAudios ? "block" : "hidden" }`}>
         Social
