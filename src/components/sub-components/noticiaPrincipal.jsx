ef5cd9adeb22b13823a18a4aee7aaaee449be505
Leonardo Diaz <jldiaz9623@gmail.com>
2023-02-08 20:31:27 -0500

    Version production

--- src/components/sub-components/noticiaPrincipal.jsx
+++ src/components/sub-components/noticiaPrincipal.jsx
@@ -9,7 +9,7 @@ const NoticiaCentral = () =>{
       <div className="grid xl:grid-cols-6 gap-4">
         <div className="grid grid-cols-1 md:col-span-2 md:grid-cols-1 gap-2 pt-5 xl:text-[1.2rem] text-[1.05rem]">
           <p className="border-b-[1.5px] border-solid border-gray-400 pb-4 xl:pb-4">Nuevos balances han contabilizado que hay al menos 2.600 personas muertas luego del sismo en el borde occidental de Europa.</p>
-          <p className="">“Garantizamos a la ONU que estamos dispuestos a ayudar y coordinar la entrega de ayuda humanitaria a todos los sirios en todo el territorio”</p>
+          <p className="">“Garantizamos a la  ONU que estamos dispuestos a ayudar y coordinar la entrega de ayuda humanitaria a todos los sirios en todo el territorio”</p>
         </div>
         <div className="flex flex-col md:col-span-4">
           <img 
